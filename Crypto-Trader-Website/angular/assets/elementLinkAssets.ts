645a1919ec129a34eb4416f4c07da07d38daf489
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-17 15:57:28 -0500

    Refactor elementLinkAssets.ts to update formatting and add `navBarAccountLink` definition in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/assets/elementLinkAssets.ts
+++ Crypto-Trader-Website/angular/assets/elementLinkAssets.ts
@@ -1,15 +1,6 @@
-import {
-    ElementLink,
-    TagType,
-    TargetType,
-    TextElementLink,
-} from '@theoliverlear/angular-suite';
+import { ElementLink, TagType, TargetType, TextElementLink } from '@theoliverlear/angular-suite'
 
-export const homeElementLink: ElementLink = new ElementLink(
-    '/',
-    TargetType.SELF,
-    false,
-);
+export const homeElementLink: ElementLink = new ElementLink('/', TargetType.SELF, false)
 
 export const navBarHomeLink: TextElementLink = new TextElementLink(
     '',
@@ -23,31 +14,23 @@ export const currenciesElementLink: ElementLink = new ElementLink(
     '/currencies',
     TargetType.SELF,
     false,
-);
+)
 
-export const traderElementLink: ElementLink = new ElementLink(
-    '/trader',
-    TargetType.SELF,
-    false,
-);
+export const traderElementLink: ElementLink = new ElementLink('/trader', TargetType.SELF, false)
 
 export const portfolioElementLink: ElementLink = new ElementLink(
     '/portfolio',
     TargetType.SELF,
     false,
-);
+)
 
-export const tradeElementLink: ElementLink = new ElementLink(
-    '/trade',
-    TargetType.SELF,
-    false,
-);
+export const tradeElementLink: ElementLink = new ElementLink('/trade', TargetType.SELF, false)
 
 export const statisticsElementLink: ElementLink = new ElementLink(
     '/statistics',
     TargetType.SELF,
     false,
-);
+)
 
 export const navBarCurrenciesTextLink: TextElementLink = new TextElementLink(
     '/currencies',
@@ -55,7 +38,7 @@ export const navBarCurrenciesTextLink: TextElementLink = new TextElementLink(
     false,
     'Currencies',
     TagType.H4,
-);
+)
 
 export const navBarPortfolioTextLink: TextElementLink = new TextElementLink(
     '/portfolio',
@@ -63,7 +46,7 @@ export const navBarPortfolioTextLink: TextElementLink = new TextElementLink(
     false,
     'Portfolio',
     TagType.H4,
-);
+)
 export const navBarTraderTextLink: TextElementLink = new TextElementLink(
     '/trader',
     TargetType.SELF,
@@ -76,7 +59,9 @@ export const simulatorElementLink: ElementLink = new ElementLink(
     '/simulator',
     TargetType.SELF,
     false,
-);
+)
+
+export const navBarAccountLink: ElementLink = new ElementLink('/account', TargetType.SELF, false)
 
 export const getStartedElementLink: TextElementLink = new TextElementLink(
     '/authorize',
@@ -84,7 +69,7 @@ export const getStartedElementLink: TextElementLink = new TextElementLink(
     false,
     'Get Started',
     TagType.H4,
-);
+)
 
 export const repositoryElementLink: TextElementLink = new TextElementLink(
     'https://github.com/Sigwarth-Software/Crypto-Trader',
@@ -92,7 +77,7 @@ export const repositoryElementLink: TextElementLink = new TextElementLink(
     false,
     'Explore Code',
     TagType.H4,
-);
+)
 
 export const docsElementLink: TextElementLink = new TextElementLink(
     'https://sigwarth-software.github.io/Crypto-Trader/',
@@ -100,12 +85,8 @@ export const docsElementLink: TextElementLink = new TextElementLink(
     false,
     'Read Docs',
     TagType.H4,
-);
+)
 
 export const upgradeElementLink: ElementLink = new ElementLink('/upgrade', TargetType.SELF, false)
 
-export const consoleElementLink: ElementLink = new ElementLink(
-    '/console',
-    TargetType.SELF,
-    false,
-);
+export const consoleElementLink: ElementLink = new ElementLink('/console', TargetType.SELF, false)
