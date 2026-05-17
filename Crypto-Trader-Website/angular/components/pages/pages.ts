4c6c24d3f3b7bd86ac9e3c794f0588a0a871ea7c
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-17 16:00:07 -0500

    Refactor pages.ts to remove semicolons for consistent formatting in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/pages/pages.ts
+++ Crypto-Trader-Website/angular/components/pages/pages.ts
@@ -1,8 +1,8 @@
-import { AccountComponent } from './account/account.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
+import { AccountComponent } from './account/account.component'
+import { HomeComponent } from './home/home.component'
+import { NotFoundComponent } from './not-found/not-found.component'
+import { StatisticsComponent } from './statistics/statistics.component'
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component'
 
 export const pages = [
     AccountComponent,
@@ -10,4 +10,4 @@ export const pages = [
     NotFoundComponent,
     StatisticsComponent,
     TermsOfServiceComponent,
-];
+]
