0e7967246ad9ddeaa98f910ca6f68ad895b072ed
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-17 15:57:57 -0500

    Refactor nav-bar.component.ts to update formatting by removing semicolons for consistency across Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bar/nav-bar.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
-import { TextElementLink } from '@theoliverlear/angular-suite';
+import { TextElementLink } from '@theoliverlear/angular-suite'
 import {
     navBarCurrenciesTextLink,
     navBarHomeLink,
     navBarPortfolioTextLink,
     navBarTraderTextLink,
-} from '@assets/elementLinkAssets';
-import { LoggedInService } from '@http/auth/status/logged-in.service';
+} from '@assets/elementLinkAssets'
+import { LoggedInService } from '@http/auth/status/logged-in.service'
 
-import { NavBarItemOption } from '../nav-bar-item/models/NavBarItemOption';
-import {
-    CryptoTraderLoggerService
-} from '@services/logging/crypto-trader-logger.service';
+import { NavBarItemOption } from '../nav-bar-item/models/NavBarItemOption'
+import { CryptoTraderLoggerService } from '@services/logging/crypto-trader-logger.service'
 
 /** A navigation bar that contains links to different pages.
  *
@@ -24,42 +22,45 @@ import {
     styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
-    protected isLoggedIn: boolean = false;
-    constructor(private readonly loggedInService: LoggedInService, private readonly log: CryptoTraderLoggerService) {}
+    protected isLoggedIn: boolean = false
+    constructor(
+        private readonly loggedInService: LoggedInService,
+        private readonly log: CryptoTraderLoggerService,
+    ) {}
 
     /** On init, listen for auth status changes and verify login status.
      *
      */
     public ngOnInit(): void {
-        this.log.setContext('NavBar');
-        this.log.info('NavBar component initialized');
-        this.listenForAuthStatus();
-        this.verifyLoginStatus();
+        this.log.setContext('NavBar')
+        this.log.info('NavBar component initialized')
+        this.listenForAuthStatus()
+        this.verifyLoginStatus()
     }
 
     private listenForAuthStatus(): void {
         this.loggedInService.getAuthState().subscribe((authStatus: boolean): void => {
-            this.log.debug(`Auth status changed: ${authStatus}`);
-            this.isLoggedIn = authStatus;
-        });
+            this.log.debug(`Auth status changed: ${authStatus}`)
+            this.isLoggedIn = authStatus
+        })
     }
 
     // TODO: Add more robust options filters.
     protected shouldShowNavItem(navBarItemOption: NavBarItemOption): boolean {
-        return this.isLoggedIn;
+        return this.isLoggedIn
     }
 
     /** Verify login status by subscribing to the logged in service.
      *
      */
     public verifyLoginStatus(): void {
-        this.log.debug('Verifying login status...');
-        this.loggedInService.isLoggedIn().subscribe();
+        this.log.debug('Verifying login status...')
+        this.loggedInService.isLoggedIn().subscribe()
     }
 
-    protected readonly navBarHomeLink: TextElementLink = navBarHomeLink;
-    protected readonly navBarPortfolioLink: TextElementLink = navBarPortfolioTextLink;
-    protected readonly navBarTraderLink: TextElementLink = navBarTraderTextLink;
-    protected readonly navBarCurrenciesLink: TextElementLink = navBarCurrenciesTextLink;
-    protected readonly NavBarItemOption: typeof NavBarItemOption = NavBarItemOption;
+    protected readonly navBarHomeLink: TextElementLink = navBarHomeLink
+    protected readonly navBarPortfolioLink: TextElementLink = navBarPortfolioTextLink
+    protected readonly navBarTraderLink: TextElementLink = navBarTraderTextLink
+    protected readonly navBarCurrenciesLink: TextElementLink = navBarCurrenciesTextLink
+    protected readonly NavBarItemOption: typeof NavBarItemOption = NavBarItemOption
 }
