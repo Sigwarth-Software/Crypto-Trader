3f08c85b968bdf9de13fe3a8f013cb2c0a8f13ec
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-17 15:49:33 -0500

    Add `simulator` page component to implement the simulator feature in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/components/pages/simulator/simulator.component.ts
@@ -0,0 +1,14 @@
+// simulator.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'simulator',
+    templateUrl: './simulator.component.html',
+    styleUrls: ['./simulator.component.scss'],
+    standalone: false
+})
+export class SimulatorComponent {
+    constructor() {
+
+    }
+}
