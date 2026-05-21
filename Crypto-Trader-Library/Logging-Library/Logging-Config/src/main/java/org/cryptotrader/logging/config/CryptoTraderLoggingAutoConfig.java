eef955ae0cab373fb9f060d51591d2bd22cd58a1
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-21 18:11:43 -0500

    Add beans for event publishing and time tracking with AspectJ support in CryptoTraderLoggingAutoConfig.java in Logging-Config

--- Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
+++ Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/org/cryptotrader/logging/config/CryptoTraderLoggingAutoConfig.java
@@ -1,15 +1,24 @@
 package org.cryptotrader.logging.config;
 
+import ch.qos.logback.classic.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.cryptotrader.logging.config.aspect.TimeTrackingAspect;
+import org.cryptotrader.logging.library.events.publisher.LogEventsPublisher;
 import org.cryptotrader.logging.properties.CryptoTraderLoggingProperties;
+import org.cryptotrader.universal.library.events.EventPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
-@ConditionalOnClass(name = "ch.qos.logback.classic.Logger")
+@ConditionalOnClass({Aspect.class, ProceedingJoinPoint.class, Logger.class})
 @EnableConfigurationProperties({CryptoTraderLoggingProperties.class})
 public class CryptoTraderLoggingAutoConfig {
 
@@ -18,5 +27,25 @@ public class CryptoTraderLoggingAutoConfig {
     @ConditionalOnProperty(prefix = "cryptotrader.exceptions", name = "enabled", havingValue = "true", matchIfMissing = true)
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnClass(StreamBridge.class)
+    @ConditionalOnBean(StreamBridge.class)
+    public EventPublisher eventPublisher(StreamBridge streamBridge) {
+        return new EventPublisher(streamBridge);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public LogEventsPublisher logEventsPublisher(@Autowired(required = false) EventPublisher eventPublisher) {
+        return new LogEventsPublisher(eventPublisher);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public TimeTrackingAspect timeTrackingAspect(@Autowired(required = false) LogEventsPublisher logEventsPublisher) {
+        return new TimeTrackingAspect(logEventsPublisher);
     }
 }
