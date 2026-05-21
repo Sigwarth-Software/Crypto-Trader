aaee73811ce852e848b6f5669065fc563ebef155
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-21 18:24:36 -0500

    Add `@TimeTracked` annotation in PortfolioTraderService.java for execution time tracking in Engine-Services

--- Crypto-Trader-Library/Engine-Library/Engine-Services/src/main/java/org/cryptotrader/engine/library/services/PortfolioTraderService.java
+++ Crypto-Trader-Library/Engine-Library/Engine-Services/src/main/java/org/cryptotrader/engine/library/services/PortfolioTraderService.java
@@ -2,19 +2,14 @@ package org.cryptotrader.engine.library.services;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.cryptotrader.api.library.entity.portfolio.PortfolioAsset;
-import org.cryptotrader.api.library.entity.portfolio.PortfolioAssetHistory;
-import org.cryptotrader.api.library.entity.portfolio.PortfolioHistory;
-import org.cryptotrader.api.library.entity.trade.TradeEvent;
-import org.cryptotrader.api.library.entity.trade.TradeType;
-import org.cryptotrader.api.library.entity.user.SubscriptionTier;
-import org.cryptotrader.api.library.model.trade.TradingEngine;
-import org.cryptotrader.api.library.services.PortfolioService;
 import org.cryptotrader.api.library.entity.portfolio.Portfolio;
+import org.cryptotrader.api.library.entity.user.SubscriptionTier;
 import org.cryptotrader.api.library.model.trade.CryptoTrader;
 import org.cryptotrader.api.library.model.trade.Trader;
-import org.cryptotrader.api.library.services.TradeEventService;
+import org.cryptotrader.api.library.model.trade.TradingEngine;
+import org.cryptotrader.api.library.services.PortfolioService;
 import org.jspecify.annotations.NonNull;
+import org.cryptotrader.universal.library.model.annotation.TimeTracked;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -32,19 +27,19 @@ import java.util.List;
 public class PortfolioTraderService {
     private final PortfolioService portfolioService;
     private List<Portfolio> allUsersPortfolios;
-    private final TradeEventService tradeEventService;
     private final CryptoTrader cryptoTrader;
     private final PlatformTransactionManager transactionManager;
+    private final PortfolioTradeExecutionService portfolioTradeExecutionService;
 
     @Autowired
     public PortfolioTraderService(PortfolioService portfolioService,
-                                  TradeEventService tradeEventService,
                                   CryptoTrader cryptoTrader,
-                                  PlatformTransactionManager transactionManager) {
+                                  PlatformTransactionManager transactionManager,
+                                  PortfolioTradeExecutionService portfolioTradeExecutionService) {
         this.portfolioService = portfolioService;
-        this.tradeEventService = tradeEventService;
         this.cryptoTrader = cryptoTrader;
         this.transactionManager = transactionManager;
+        this.portfolioTradeExecutionService = portfolioTradeExecutionService;
     }
 
     @PostConstruct
@@ -71,7 +66,6 @@ public class PortfolioTraderService {
 
     //--------------------------Trade-Portfolios------------------------------
     public void tradePortfolios(List<Portfolio> portfolios, SubscriptionTier subscriptionTier) {
-        // TODO: Modularize logging formatting for tiers.
         final String logPrefix = getLogPrefix(subscriptionTier);
         if (portfolios == null || portfolios.isEmpty()) {
             log.info("{} No traders found. No trades will be made.", logPrefix);
@@ -82,7 +76,7 @@ public class PortfolioTraderService {
         this.triggerAllTraders(this.cryptoTrader.getTradersBySubscriptionTier(subscriptionTier));
     }
 
-    private static @NonNull String getLogPrefix(SubscriptionTier subscriptionTier) {
+    static @NonNull String getLogPrefix(SubscriptionTier subscriptionTier) {
         final String resetAnsi = "\u001B[0m";
         final String purpleAnsi = "\u001B[35m";
         final String greenAnsi = "\u001B[32m";
@@ -138,105 +132,13 @@ public class PortfolioTraderService {
         this.triggerAllTraders(this.cryptoTrader.getTraders());
     }
 
+    @TimeTracked(expectedMillis = 2000, shouldPersist = true)
     public void triggerAllTraders(List<Trader> traders) {
         for (Trader trader : traders) {
             for (TradingEngine assetTrader : trader.getAssetTraders()) {
-                this.triggerTrader(trader, assetTrader);
+                this.portfolioTradeExecutionService.executeTrader(trader, assetTrader);
             }
         }
-    }
-
-    private void triggerTrader(Trader trader, TradingEngine assetTrader) {
-        PortfolioAsset traderAsset = assetTrader.getAsset();
-        PortfolioAsset previousAsset = PortfolioAsset.from(traderAsset);
-        boolean tradeOccurred = assetTrader.trade();
-        if (tradeOccurred) {
-            final String logPrefix = getLogPrefix(trader.getPortfolio().getUser().getSubscriptionTier());
-            log.info("{} Trade executed for trader {} on asset {}. Shares: {} {}, Wallet Dollars: {}, Target Price: {}.",
-                     logPrefix,
-                     trader.getPortfolio().getUser().getId(),
-                     traderAsset.getCurrency().getCurrencyCode(),
-                     traderAsset.getShares(),
-                     traderAsset.getCurrency().getCurrencyCode(),
-                     traderAsset.getAssetWalletDollars(),
-                     traderAsset.getTargetPrice());
-        }
-        updateTraders(trader, assetTrader);
-        if (hasAssetChanged(previousAsset, traderAsset)) {
-            this.saveAssetChanges(trader, traderAsset, tradeOccurred);
-        }
-    }
-
-    private void saveAssetChanges(Trader trader, PortfolioAsset traderAsset, boolean tradeOccurred) {
-        PortfolioAssetHistory portfolioAssetHistory = new PortfolioAssetHistory(traderAsset, tradeOccurred);
-        PortfolioAssetHistory previousPortfolioAssetHistory = this.portfolioService.getLatestPortfolioAssetHistory(traderAsset);
-        PortfolioAssetHistory previousWithShares = this.getLastPortfolioAssetWithSharesSinceTime(portfolioAssetHistory);
-        this.portfolioService.setPortfolioValueChange(previousPortfolioAssetHistory, portfolioAssetHistory);
-        this.portfolioService.setPortfolioSharesChange(previousWithShares, portfolioAssetHistory);
-        traderAsset.addPortfolioAssetHistory(portfolioAssetHistory);
-        Portfolio traderPortfolio = trader.getPortfolio();
-        PortfolioHistory previousPortfolioHistory = this.portfolioService.getLatestPortfolioHistory(traderPortfolio);
-        PortfolioHistory portfolioHistory = new PortfolioHistory(traderPortfolio, tradeOccurred);
-        this.setValueChange(previousPortfolioHistory, portfolioHistory);
-        traderPortfolio.addPortfolioHistory(portfolioHistory);
-        this.saveAll(traderAsset, traderPortfolio, portfolioAssetHistory, portfolioHistory);
-        if (tradeOccurred) {
-            this.saveTradeEvent(portfolioAssetHistory);
-        }
-    }
-
-    private static boolean hasAssetChanged(PortfolioAsset previousAsset, PortfolioAsset traderAsset) {
-        return !previousAsset.equals(traderAsset);
-    }
-
-    private static void updateTraders(Trader trader, TradingEngine assetTrader) {
-        assetTrader.getAsset().updateValues();
-        trader.getPortfolio().updateValues();
-    }
-
-    private void saveTradeEvent(PortfolioAssetHistory portfolioAssetHistory) {
-        TradeType tradeType = TradeEvent.getTradeType(portfolioAssetHistory);
-        TradeEvent tradeEvent = new TradeEvent(portfolioAssetHistory,
-                                               tradeType,
-                                               portfolioAssetHistory.getValueChange(),
-                                               portfolioAssetHistory.getSharesChange());
-        this.tradeEventService.saveTradeEvent(tradeEvent);
-    }
-
-
-    private void setValueChange(PortfolioHistory previousPortfolioHistory, PortfolioHistory portfolioHistory) {
-        if (previousPortfolioHistory != null) {
-            portfolioHistory.calculateValueChange(previousPortfolioHistory);
-        } else {
-            portfolioHistory.setValueChange(0);
-        }
-    }
-
-    private void setSharesChange(PortfolioAssetHistory portfolioHistory) {
-        PortfolioAssetHistory historyWithShares = getLastPortfolioAssetWithSharesSinceTime(portfolioHistory);
-        if (historyWithShares != null) {
-            portfolioHistory.calculateShareChange(historyWithShares);
-        } else {
-            portfolioHistory.setSharesChange(0);
-        }
-    }
-
-    /**
-     * Returns the most recent preceding history entry for the same asset where shares != 0.
-     * Used as the baseline for calculating deltas on a new history entry.
-     */
-    private PortfolioAssetHistory getLastPortfolioAssetWithSharesSinceTime(PortfolioAssetHistory assetToLookRetrospectively) {
-        return this.portfolioService.getLatestPreviousAssetHistoryWithShares(assetToLookRetrospectively);
-    }
-
-    private void saveAll(PortfolioAsset traderAsset,
-                         Portfolio traderPortfolio,
-                         PortfolioAssetHistory portfolioAssetHistory,
-                         PortfolioHistory portfolioHistory) {
-        this.portfolioService.savePortfolioAsset(traderAsset);
-        this.portfolioService.savePortfolio(traderPortfolio);
-        this.portfolioService.savePortfolioAssetHistory(portfolioAssetHistory);
-        this.portfolioService.savePortfolioHistory(portfolioHistory);
     }
 
     //----------------------Add-Portfolio-To-Traders--------------------------
