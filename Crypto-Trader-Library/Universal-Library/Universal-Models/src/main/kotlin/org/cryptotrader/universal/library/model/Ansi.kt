de0980a33ab1cc305096275bf0c7043dec2fd8b9
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-21 17:56:17 -0500

    Add Ansi.kt to define ANSI color codes as constants for universal styling in Universal-Models

---
+++ Crypto-Trader-Library/Universal-Library/Universal-Models/src/main/kotlin/org/cryptotrader/universal/library/model/Ansi.kt
@@ -0,0 +1,15 @@
+package org.cryptotrader.universal.library.model
+
+object Ansi {
+    const val RESET = "\u001B[0m"
+    const val UNDERLINE = "\u001B[4m"
+    const val BOLD = "\u001B[1m"
+    const val CYAN = "\u001B[36m"
+    const val MAGENTA = "\u001B[35m"
+    const val BLUE = "\u001B[34m"
+    const val GREEN = "\u001B[32m"
+    const val YELLOW = "\u001B[33m"
+    const val WHITE = "\u001B[37m"
+    const val RED = "\u001B[31m"
+    const val GRAY = "\u001B[90m"
+}
