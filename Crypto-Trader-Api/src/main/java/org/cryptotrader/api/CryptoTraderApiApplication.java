d36f0bc4a376df55a93b279fd2dbc0c701cac94b
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-21 17:59:58 -0500

    Add `@EnableLoadTimeWeaving` annotation to support AspectJ weaving in CryptoTraderApiApplication.java for Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/CryptoTraderApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
 @ConfigurationPropertiesScan
 @EntityScan(basePackages = {
         "org.cryptotrader.api.library.entity",
