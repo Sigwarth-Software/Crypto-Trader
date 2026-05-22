152f2c93aac906f1a5b4cb9904259f5730f1595a
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-21 19:10:52 -0500

    Add type alias for `createConsumer()` return type for clarity in BaseGatewayConfig.kt for Universal-Events

--- Crypto-Trader-Library/Universal-Library/Universal-Events/src/main/kotlin/org/cryptotrader/universal/library/events/config/BaseGatewayConfig.kt
+++ Crypto-Trader-Library/Universal-Library/Universal-Events/src/main/kotlin/org/cryptotrader/universal/library/events/config/BaseGatewayConfig.kt
@@ -1,8 +1,8 @@
 package org.cryptotrader.universal.library.events.config
 
 import org.cryptotrader.universal.library.events.RequestGatewayController
+import org.cryptotrader.universal.library.events.alias.MessageHandler
 import org.slf4j.Logger
-import org.springframework.messaging.Message
 import java.util.function.Consumer
 
 abstract class BaseGatewayConfig<Request, Response>(
@@ -10,7 +10,7 @@ abstract class BaseGatewayConfig<Request, Response>(
 ) {
     protected abstract val log: Logger
 
-    protected fun createConsumer(): Consumer<Message<Response>> {
+    protected fun createConsumer(): MessageHandler<Response> {
         return Consumer { message ->
             val correlationId = message.headers["correlationId"]
             log.debug("Dispatching response to gateway handler: correlationId={}", correlationId)
