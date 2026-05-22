499ba2ab549a488853ea1817c008a04f01fde31f
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-22 06:41:04 -0500

    Add entry to LogEventBinding.kt for handling execution speed log events in Logging-Events

--- Crypto-Trader-Library/Logging-Library/Logging-Events/src/main/kotlin/org/cryptotrader/logging/library/events/LogEventBinding.kt
+++ Crypto-Trader-Library/Logging-Library/Logging-Events/src/main/kotlin/org/cryptotrader/logging/library/events/LogEventBinding.kt
@@ -3,5 +3,6 @@ package org.cryptotrader.logging.library.events
 import org.cryptotrader.universal.library.events.model.EventBinding
 
 enum class LogEventBinding(override val bindingName: String) : EventBinding {
-    FRONTEND_LOGS_REQUESTS("frontendLogs-out-0");
+    FRONTEND_LOGS_REQUESTS("frontendLogs-out-0"),
+    EXECUTION_SPEED_LOGS_REQUESTS("executionSpeedLogs-out-0");
 }
