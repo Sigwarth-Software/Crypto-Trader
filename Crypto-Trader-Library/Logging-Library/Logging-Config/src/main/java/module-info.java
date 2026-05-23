6bde08dc646d636f8e78d10cecee08eaf568e479
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-23 17:52:59 -0500

    Add module dependencies for logging, events, and aspect configuration in module-info.java for Logging-Config

--- Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/module-info.java
+++ Crypto-Trader-Library/Logging-Library/Logging-Config/src/main/java/module-info.java
@@ -1,22 +1,30 @@
 open module org.cryptotrader.logging.library.config {
+    requires org.cryptotrader.universal.library.models;
+    requires org.cryptotrader.logging.library.events;
+    requires org.cryptotrader.universal.library.events;
     requires spring.boot.autoconfigure;
     requires spring.boot;
     requires spring.context;
     requires spring.beans;
+    requires spring.aop;
+    requires spring.cloud.stream;
     requires spring.core;
     requires spring.web;
     requires spring.messaging;
     requires spring.websocket;
     requires org.apache.tomcat.embed.core;
+    requires org.aspectj.weaver;
     requires ch.qos.logback.classic;
     requires ch.qos.logback.core;
     requires static lombok;
     requires org.slf4j;
     requires com.fasterxml.jackson.databind;
+    requires org.jetbrains.annotations;
 
     exports org.cryptotrader.logging.config;
     exports org.cryptotrader.logging.http;
     exports org.cryptotrader.logging.logback;
     exports org.cryptotrader.logging.properties;
     exports org.cryptotrader.logging.websocket;
+    exports org.cryptotrader.logging.config.aspect;
 }
