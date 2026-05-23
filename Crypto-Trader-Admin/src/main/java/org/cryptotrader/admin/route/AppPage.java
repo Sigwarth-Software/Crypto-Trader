4e46365f6c296728311a0ec79a19489adb20ddfc
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-23 17:32:21 -0500

    Add constants for user, ban, and warn offense tables in AppPage.java to expand table navigation options in Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/route/AppPage.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/route/AppPage.java
@@ -5,7 +5,10 @@ public enum AppPage {
     AUTH("Authorize"),
     DASHBOARD("Dashboard"),
     EMAIL("Email"),
-    TABLES("Tables");
+    TABLES("Tables"),
+    TABLE_USER_DATA("User Data Table"),
+    TABLE_BAN_OFFENSES("Ban Offenses Table"),
+    TABLE_WARN_OFFENSES("Warn Offenses Table");
     public final String pageName;
     AppPage(String pageName) {
         this.pageName = pageName;
