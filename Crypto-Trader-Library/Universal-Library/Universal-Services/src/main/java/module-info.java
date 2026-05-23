00b477b03827f40bff297f91a8ce9051b446a1c0
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-23 18:54:38 -0500

    Add `jakarta.persistence` dependency in module-info.java for Universal-Services

--- Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
+++ Crypto-Trader-Library/Universal-Library/Universal-Services/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 open module org.cryptotrader.universal.library.services {
     requires kotlin.stdlib;
+    requires jakarta.persistence;
     requires org.cryptotrader.universal.library.models;
     requires spring.data.jpa;
     requires org.slf4j;
