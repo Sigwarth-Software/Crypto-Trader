5a00cb15db3d7f826dffb73264ecb19e96d3facb
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-05-23 16:00:51 -0500

    Add BanOffensesTableController.kt to initialize and configure ban offenses table in Crypto-Trader-Admin

---
+++ Crypto-Trader-Admin/src/main/kotlin/org/cryptotrader/admin/controller/BanOffensesTableController.kt
@@ -0,0 +1,28 @@
+package org.cryptotrader.admin.controller
+
+import javafx.collections.FXCollections
+import javafx.fxml.FXML
+import javafx.scene.control.TableColumn
+import javafx.scene.control.cell.PropertyValueFactory
+import org.cryptotrader.admin.library.model.BanOffense
+import org.cryptotrader.desktop.library.component.ui.DataTable
+import org.springframework.stereotype.Component
+
+@Component
+class BanOffensesTableController {
+
+    @FXML
+    lateinit var banOffensesDataTable: DataTable<BanOffense>
+
+    @FXML
+    fun initialize() {
+        val nameCol = TableColumn<BanOffense, String>("Offense Name")
+        nameCol.cellValueFactory = PropertyValueFactory("nameString")
+
+        val descriptionCol = TableColumn<BanOffense, String>("Description")
+        descriptionCol.cellValueFactory = PropertyValueFactory("description")
+
+        banOffensesDataTable.setColumns(listOf(nameCol, descriptionCol))
+        banOffensesDataTable.setItems(FXCollections.observableArrayList(*BanOffense.values()))
+    }
+}
