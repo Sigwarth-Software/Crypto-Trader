72899e33becdd8d40c31377db7827b86e8010100
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-05 19:22:02 -0500

    Add `IntelligencePanelComponent` to pages array in pages.ts for Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/pages/pages.ts
+++ Crypto-Trader-Website/angular/components/pages/pages.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component'
 import { NotFoundComponent } from './not-found/not-found.component'
 import { StatisticsComponent } from './statistics/statistics.component'
 import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component'
+import { IntelligencePanelComponent } from "./intelligence-panel/intelligence-panel.component";
 
 export const pages = [
     AccountComponent,
     HomeComponent,
+    IntelligencePanelComponent,
     NotFoundComponent,
     StatisticsComponent,
-    TermsOfServiceComponent,
+    TermsOfServiceComponent
 ]
