997445dd7e9eb0422ab075c76994ae7accaf06f5
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-14 17:19:53 -0500

    Add `SimulationBuilderComponent` to simulator.module.ts for Crypto-Trader-Website

--- Crypto-Trader-Website/angular/modules/feature/simulator.module.ts
+++ Crypto-Trader-Website/angular/modules/feature/simulator.module.ts
@@ -6,8 +6,12 @@ import { UniversalModule } from '@modules/shared/universal.module'
 import {
     SimulatorComponent
 } from '@components/pages/simulator/simulator.component'
+import {
+    SimulationBuilderComponent
+} from "@components/elements/element-group-simulator/simulation-builder/simulation-builder.component";
 
 const simulatorComponents: any[] = [
+    SimulationBuilderComponent,
     SimulatorComponent,
 ]
 
