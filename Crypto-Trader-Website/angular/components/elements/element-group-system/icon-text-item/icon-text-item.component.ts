c1414e36b88262ee8b2ec93664d5ca0f3827c54b
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-14 17:46:00 -0500

    Add `invertColor` property and `HostBinding` for conditional styling in icon-text-item.component.ts in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-system/icon-text-item/icon-text-item.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-system/icon-text-item/icon-text-item.component.ts
@@ -1,5 +1,5 @@
 // icon-text-item.component.ts
-import { Component, Input } from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 import { ImageAsset } from '@assets/imageAssets';
 
 @Component({
@@ -9,7 +9,11 @@ import { ImageAsset } from '@assets/imageAssets';
     styleUrls: ['./icon-text-item.component.scss'],
 })
 export class IconTextItemComponent {
+    @HostBinding('class.use-invert-color') get invertColorClass(): boolean {
+        return this.invertColor;
+    }
     // TODO: Remove the non-null assertion.
     @Input() public icon!: ImageAsset;
     @Input() public text: string = '';
+    @Input() public invertColor: boolean = false;
 }
