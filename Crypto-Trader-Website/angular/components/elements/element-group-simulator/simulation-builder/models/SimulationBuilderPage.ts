cfd1df0883a028b2d56ab763e3d64951d54dcc70
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-19 16:24:12 -0500

    Add TODO comment for implementing user portfolio and persona presets in SimulationBuilderPage.ts

--- Crypto-Trader-Website/angular/components/elements/element-group-simulator/simulation-builder/models/SimulationBuilderPage.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-simulator/simulation-builder/models/SimulationBuilderPage.ts
@@ -3,6 +3,10 @@
  */
 export enum SimulationBuilderPage {
     Welcome = 'Welcome',
+    // TODO: Add features for authenticated users to use their current
+    //       portfolio or use the user personas on the homepage with set
+    //       assets.
+    Presets = 'Presets',
     SelectFeatures = 'Select Features',
     AddCurrency = 'Add Currency',
     Review = 'Review',
