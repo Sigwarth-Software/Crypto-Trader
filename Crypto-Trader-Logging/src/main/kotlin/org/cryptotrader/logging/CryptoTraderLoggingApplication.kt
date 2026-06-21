1662510f7d8615a2ce7c0ffefd612ca4a96d9263
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-20 19:07:19 -0500

    Enable JPA repositories and entity scanning for library integration in CryptoTraderLoggingApplication.kt

--- Crypto-Trader-Logging/src/main/kotlin/org/cryptotrader/logging/CryptoTraderLoggingApplication.kt
+++ Crypto-Trader-Logging/src/main/kotlin/org/cryptotrader/logging/CryptoTraderLoggingApplication.kt
@@ -1,9 +1,13 @@
 package org.cryptotrader.logging
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["org.cryptotrader.logging.library.repository"])
+@EntityScan(basePackages = ["org.cryptotrader.logging.library.entity"])
 open class CryptoTraderLoggingApplication
 
 fun main(args: Array<String>) {
