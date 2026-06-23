2af1cca16f480e5e10039e2dea5a2ba1a30138ee
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-23 17:18:17 -0500

    Add descriptive strings in DownsampleDuration.kt for Simulator-Models

--- Crypto-Trader-Library/Simulator-Library/Simulator-Models/src/main/kotlin/org/cryptotrader/simulator/library/model/DownsampleDuration.kt
+++ Crypto-Trader-Library/Simulator-Library/Simulator-Models/src/main/kotlin/org/cryptotrader/simulator/library/model/DownsampleDuration.kt
@@ -1,8 +1,8 @@
 package org.cryptotrader.simulator.library.model
 
-enum class DownsampleDuration {
-    MINUTE,
-    HOUR,
-    DAY,
-    MONTH,
+enum class DownsampleDuration(val durationString: String) {
+    MINUTE("minute"),
+    HOUR("hour"),
+    DAY("day"),
+    MONTH("month"),
 }
