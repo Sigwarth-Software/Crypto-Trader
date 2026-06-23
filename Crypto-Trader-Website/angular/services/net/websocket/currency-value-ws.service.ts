55187ee346799b8a9cc7693c289f0499ccac636e
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2026-06-22 19:41:14 -0500

    Refactor currency-value-ws.service.ts to use `resolveWebSocketUrl()` for constructing WebSocket URLs in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/services/net/websocket/currency-value-ws.service.ts
+++ Crypto-Trader-Website/angular/services/net/websocket/currency-value-ws.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
-import { WebSocketService } from '@theoliverlear/angular-suite';
-import { environment } from '@environments/environment';
+import { WebSocketService } from '@theoliverlear/angular-suite'
+import { environment } from '@environments/environment'
+import { resolveWebSocketUrl } from '@services/net/url-resolver'
 
 @Injectable({
     providedIn: 'root',
 })
 export class CurrencyValueWsService extends WebSocketService<string, string> {
-    private static readonly URL: string = `${environment.websocketUrl}/currency/value`;
+    private static readonly URL: string = resolveWebSocketUrl(
+        `${environment.websocketUrl}/currency/value`,
+    )
 
     constructor() {
-        super(CurrencyValueWsService.URL);
+        super(CurrencyValueWsService.URL)
     }
 }
