aed2f22199a90f0c9c329ca96f31fbb4ee57c72e
theoliverlear <sigwartholiver2@gmail.com>
2026-06-25 06:28:50 -0500

    Refactor ConsoleRequestGateway.kt to remove unused imports and unused `handleConsoleResponse()` method in Console-Components

--- Crypto-Trader-Library/Console-Library/Console-Components/src/main/kotlin/org/cryptotrader/console/library/component/ConsoleRequestGateway.kt
+++ Crypto-Trader-Library/Console-Library/Console-Components/src/main/kotlin/org/cryptotrader/console/library/component/ConsoleRequestGateway.kt
@@ -7,7 +7,6 @@ import org.cryptotrader.universal.library.events.RequestGatewayController
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.messaging.Message
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,9 +14,4 @@ class ConsoleRequestGateway @Autowired constructor(
     override val eventPublisher: EventPublisher
 ) : RequestGatewayController<ConsoleCommandRequest, ConsoleCommandResponse>() {
     private val log: Logger = LoggerFactory.getLogger(ConsoleRequestGateway::class.java)
-
-    fun handleConsoleResponse(message: Message<ConsoleCommandResponse>) {
-        log.info("Handling console response correlationId={}", message.headers["correlationId"])
-        this.handleResponse(message)
-    }
 }
