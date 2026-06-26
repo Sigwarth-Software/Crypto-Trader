3da55d12375f96835d0ca342e5046415956c20f9
theoliverlear <sigwartholiver2@gmail.com>
2026-06-26 17:56:00 -0500

    Update SubscriptionTier.kt to adjust interval values for subscription tiers in Api-Models

--- Crypto-Trader-Library/Api-Library/Api-Models/src/main/kotlin/org/cryptotrader/api/library/entity/user/SubscriptionTier.kt
+++ Crypto-Trader-Library/Api-Library/Api-Models/src/main/kotlin/org/cryptotrader/api/library/entity/user/SubscriptionTier.kt
@@ -1,9 +1,9 @@
 package org.cryptotrader.api.library.entity.user
 
 enum class SubscriptionTier(val label: String, val level: Int, val intervalMs: Long) {
-    FREE("Free", 1, 10000L),
-    PRO("Pro", 2, 5000L),
-    ULTIMATE("Ultimate", 3, 1000L);
+    FREE("Free", 1, 30000L),
+    PRO("Pro", 2,  1000L),
+    ULTIMATE("Ultimate", 3, 5000L);
 
     companion object {
         fun fromLabel(label: String): SubscriptionTier? {
