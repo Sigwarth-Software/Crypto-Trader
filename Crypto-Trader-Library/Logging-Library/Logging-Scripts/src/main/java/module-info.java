48bb3f27dadf42985140d12f1e7e33ba8e906c3b
theoliverlear <sigwartholiver2@gmail.com>
2026-06-26 17:24:22 -0500

    Correct universal library path and add Spring dependencies to module-info.java for Logging-Scripts

--- Crypto-Trader-Library/Logging-Library/Logging-Scripts/src/main/java/module-info.java
+++ Crypto-Trader-Library/Logging-Library/Logging-Scripts/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.cryptotrader.logging.library.scripts {
     requires kotlin.stdlib;
-    requires org.cryptotrader.universal.models;
+    requires spring.boot;
+    requires spring.core;
+    requires spring.web;
+    requires org.cryptotrader.universal.library.models;
 
     exports org.cryptotrader.logging.library.scripts;
 }
