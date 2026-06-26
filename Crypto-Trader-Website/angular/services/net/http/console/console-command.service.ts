a3be7ae0d2125cc0db0f01a2285b9c99fcd74e23
theoliverlear <sigwartholiver2@gmail.com>
2026-06-26 17:26:57 -0500

    Refactor console-command.service.ts to use URL resolver for HTTP endpoint and remove semicolons for style consistency in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/services/net/http/console/console-command.service.ts
+++ Crypto-Trader-Website/angular/services/net/http/console/console-command.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core'
+import { environment } from '@environments/environment'
+import { Observable } from 'rxjs'
 
-import { HttpClientService } from '@theoliverlear/angular-suite';
-import { ConsoleCommandRequest, ConsoleCommandResponse } from '@models/console/types';
-import { CryptoTraderLoggerService } from '@services/logging/crypto-trader-logger.service';
-import {LoggerContext} from "@models/logging/LoggerContext";
+import { HttpClientService } from '@theoliverlear/angular-suite'
+import { ConsoleCommandRequest, ConsoleCommandResponse } from '@models/console/types'
+import { CryptoTraderLoggerService } from '@services/logging/crypto-trader-logger.service'
+import { LoggerContext } from '@models/logging/LoggerContext'
+import { resolveAbsoluteHttpUrl } from '@services/net/url-resolver'
 
 /** A service for executing console commands by sending HTTP requests.
  *
@@ -17,11 +18,13 @@ export class ConsoleCommandService extends HttpClientService<
     ConsoleCommandRequest,
     ConsoleCommandResponse
 > {
-    private static readonly URL: string = `${environment.apiUrl}/console/execute`;
+    private static readonly URL: string = resolveAbsoluteHttpUrl(
+        `${environment.apiUrl}/console/execute`,
+    )
 
     constructor(private readonly log: CryptoTraderLoggerService) {
-        super(ConsoleCommandService.URL);
-        this.log.setContext(LoggerContext.Console);
+        super(ConsoleCommandService.URL)
+        this.log.setContext(LoggerContext.Console)
     }
 
     /** Executes a console command by sending a POST request to the server
@@ -30,7 +33,7 @@ export class ConsoleCommandService extends HttpClientService<
      * @param command
      */
     public executeCommand(command: ConsoleCommandRequest): Observable<ConsoleCommandResponse> {
-        this.log.info(`Executing: ${command.commandText}`);
-        return this.post(command);
+        this.log.info(`Executing: ${command.commandText}`)
+        return this.post(command)
     }
 }
