ae71fddc7ded4a81a08483d97431e4df07f5ca33
theoliverlear <sigwartholiver2@gmail.com>
2026-06-26 17:23:46 -0500

    Expand ServiceStatusChecker.kt to support HTTPS health checks with configurable CA bundles for improved security in Health-Models

--- Crypto-Trader-Library/Health-Library/Health-Models/src/main/kotlin/org/cryptotrader/health/library/model/ServiceStatusChecker.kt
+++ Crypto-Trader-Library/Health-Library/Health-Models/src/main/kotlin/org/cryptotrader/health/library/model/ServiceStatusChecker.kt
@@ -4,13 +4,25 @@ package org.cryptotrader.health.library.model
 import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
 import org.apache.http.impl.client.HttpClientBuilder
 import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
 import java.net.URI
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 private const val BASE_OVERRIDE_PROP = "ct.health.base"
 private const val BASE_OVERRIDE_ENV = "CT_HEALTH_BASE"
+private const val CA_BUNDLE_PROP = "ct.health.caBundle"
+private const val CA_BUNDLE_ENV = "CT_HEALTH_CA_BUNDLE"
+private const val SHARED_CA_BUNDLE_ENV = "CT_CA_BUNDLE"
 
 private val log = LoggerFactory.getLogger("org.cryptotrader.health.ServiceStatus")
 
@@ -24,7 +36,7 @@ internal fun getUrl(service: CryptoTraderService): String {
         return "https://sigwarth-software.github.io/Crypto-Trader/"
     }
     val host: String = if (service === CryptoTraderService.DATA) {
-        System.getenv("CT_DATA_HOST")
+        System.getenv("CT_DATA_HOST") ?: "localhost"
     } else {
         "localhost"
     }
@@ -32,7 +44,7 @@ internal fun getUrl(service: CryptoTraderService): String {
     return if (base != null) {
         URI.create(base).resolve("/actuator/health").toString()
     } else {
-        "http://${host}:${service.port}/actuator/health"
+        "https://${host}:${service.port}/actuator/health"
     }
 }
 
@@ -41,20 +53,53 @@ private fun getHttpRequest(url: String): HttpGet {
 }
 
 private fun getHttpClient(): HttpClient {
-    return HttpClientBuilder.create().build()
+    val caBundle: String? = getCaBundle()
+    if (caBundle.isNullOrBlank()) {
+        return HttpClientBuilder.create().build()
+    }
+    return HttpClientBuilder.create()
+        .setSSLSocketFactory(getSslSocketFactory(caBundle))
+        .build()
+}
+
+private fun getCaBundle(): String? {
+    return System.getProperty(CA_BUNDLE_PROP)
+        ?: System.getenv(CA_BUNDLE_ENV)
+        ?: System.getenv(SHARED_CA_BUNDLE_ENV)
+}
+
+private fun getSslSocketFactory(caBundle: String): SSLConnectionSocketFactory {
+    val certificateFile = File(caBundle)
+    require(certificateFile.isFile) {
+        "Configured health check CA bundle does not exist: ${certificateFile.absolutePath}"
+    }
+    val certificateFactory: CertificateFactory = CertificateFactory.getInstance("X.509")
+    val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+    keyStore.load(null, null)
+    FileInputStream(certificateFile).use { inputStream: FileInputStream ->
+        val certificates: Collection<Certificate> = certificateFactory.generateCertificates(inputStream)
+        certificates.forEachIndexed { index: Int, certificate: Certificate ->
+            keyStore.setCertificateEntry("ct-health-ca-$index", certificate as X509Certificate)
+        }
+    }
+    val trustManagerFactory: TrustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(keyStore)
+    val sslContext: SSLContext = SSLContext.getInstance("TLS")
+    sslContext.init(null, trustManagerFactory.trustManagers, null)
+    return SSLConnectionSocketFactory(sslContext)
 }
 
 fun isServiceAlive(service: CryptoTraderService): Boolean {
     log.info("Checking status of service: {}...", service)
-    val validCode: Int = 200
-    val client: HttpClient = getHttpClient()
-    val request: HttpGet = getHttpRequest(getUrl(service))
+    val validCode = 200
     try {
+        val client: HttpClient = getHttpClient()
+        val request: HttpGet = getHttpRequest(getUrl(service))
         val response: HttpResponse = client.execute(request) ?: return false
         val isAlive: Boolean = validCode == response.statusLine.statusCode
         log.info("Service: {} is alive: {}", service, isAlive)
         return isAlive
-    } catch (exception: IOException) {
+    } catch (exception: Exception) {
         log.error("Error checking status of service: {}", service, exception)
         return false
     }
