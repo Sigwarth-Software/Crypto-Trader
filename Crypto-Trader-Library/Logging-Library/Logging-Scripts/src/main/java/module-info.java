6b401744714c6bb31850423bd6252aec12ad9680
theoliverlear <sigwartholiver2@gmail.com>
2026-06-26 06:40:51 -0500

    Add module-info.java to define Logging-Scripts module in Logging-Library

---
+++ Crypto-Trader-Library/Logging-Library/Logging-Scripts/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.cryptotrader.logging.library.scripts {
+    requires kotlin.stdlib;
+    requires org.cryptotrader.universal.models;
+
+    exports org.cryptotrader.logging.library.scripts;
+}
