24bf369eb964521344c607c83777b6dd03d8f986
theoliverlear <sigwartholiver2@gmail.com>
2026-06-27 12:04:07 -0500

    Remove extraneous system property in CryptoTraderDataApplication.java for Crypto-Trdaer-Data

--- Crypto-Trader-Data/src/main/java/org/cryptotrader/data/CryptoTraderDataApplication.java
+++ Crypto-Trader-Data/src/main/java/org/cryptotrader/data/CryptoTraderDataApplication.java
@@ -38,7 +38,6 @@ public class CryptoTraderDataApplication {
             CurrencyJsonGenerator.standalone().generateAndSave();
         }
         enableCurrencyHarvesting();
-        System.setProperty("cryptotrader.entity.service.silent", "true");
         SpringApplication.run(CryptoTraderDataApplication.class, args);
     }
 
