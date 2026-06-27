757eeb3af3cd911576151fd67d68a73a21dfcbf4
theoliverlear <sigwartholiver2@gmail.com>
2026-06-27 15:55:08 -0500

    Expand ExecutionSpeedLogConsumerConfig.kt to include configurable warning level calculation using `TimeTrackingProperties` and persist execution speed logs with warning levels

--- Crypto-Trader-Logging/src/main/kotlin/org/cryptotrader/logging/config/ExecutionSpeedLogConsumerConfig.kt
+++ Crypto-Trader-Logging/src/main/kotlin/org/cryptotrader/logging/config/ExecutionSpeedLogConsumerConfig.kt
@@ -1,7 +1,9 @@
 package org.cryptotrader.logging.config
 
 import org.cryptotrader.logging.library.events.ExecutionSpeedLogEventPayload
+import org.cryptotrader.logging.library.entity.ExecutionSpeedWarningLevel
 import org.cryptotrader.logging.library.service.ExecutionSpeedLogService
+import org.cryptotrader.logging.properties.TimeTrackingProperties
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,16 +12,25 @@ import java.util.function.Consumer
 
 @Configuration
 open class ExecutionSpeedLogConsumerConfig(
-    private val persistenceService: ExecutionSpeedLogService
+    private val persistenceService: ExecutionSpeedLogService,
+    private val timeTrackingProperties: TimeTrackingProperties,
 ) {
     private val log = LoggerFactory.getLogger(ExecutionSpeedLogConsumerConfig::class.java)
 
     @Bean(name = ["executionSpeedLogsConsumer"])
     open fun executionSpeedLogsConsumer(): Consumer<Message<ExecutionSpeedLogEventPayload>> {
         return Consumer { message ->
-            val entry = message.payload
+            val entry: ExecutionSpeedLogEventPayload = message.payload
             log.debug("Received execution speed log for {}", entry.fullMethodQualifiedName)
-            persistenceService.persist(entry)
+            val warningLevel = ExecutionSpeedWarningLevel.from(
+                entry.executionSpeed,
+                entry.expectedExecutionSpeed,
+                timeTrackingProperties.exceedingFactor,
+                timeTrackingProperties.expectedFactor,
+                timeTrackingProperties.warningFactor,
+                timeTrackingProperties.alertFactor,
+            )
+            persistenceService.persist(entry, warningLevel)
         }
     }
 }
