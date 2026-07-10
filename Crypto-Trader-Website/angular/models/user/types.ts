7913c29b0aa43afff5bf4fd991a8f676437877ca
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 06:23:34 -0500

    Expand types.ts to add `SubscriptionTierResponse` in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/models/user/types.ts
+++ Crypto-Trader-Website/angular/models/user/types.ts
@@ -1,1 +1,7 @@
-export type SubscriptionTier = 'FREE' | 'PRO' | 'ULTIMATE';
+// TODO: Refactor to enum.
+
+export type SubscriptionTier = 'FREE' | 'PRO' | 'ULTIMATE'
+
+export type SubscriptionTierResponse = {
+    subscriptionTier: SubscriptionTier
+}
