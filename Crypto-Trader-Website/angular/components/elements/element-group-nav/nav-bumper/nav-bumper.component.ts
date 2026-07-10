0144c500c7e67b6ae8b1f027cab8953395955534
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 06:21:36 -0500

    Refactor nav-bumper.component.ts to update service names and add logging context in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bumper/nav-bumper.component.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-nav/nav-bumper/nav-bumper.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CryptoTraderLoggerService } from '@app/services/logging/crypto-trader-logger.service'
 
 import { LoggedInService } from '@http/auth/status/logged-in.service';
-import { MySubscriptionTierService } from '@http/user/my-subscription-tier.service';
-import { SubscriptionTier } from '@models/user/types';
+import { SubscriptionTierService } from '@http/user/subscription-tier.service'
+import { SubscriptionTierResponse } from '@models/user/types'
+import { LoggerContext } from '@models/logging/LoggerContext'
 
 @Component({
     selector: 'nav-bumper',
@@ -11,27 +13,32 @@ import { SubscriptionTier } from '@models/user/types';
     standalone: false,
 })
 export class NavBumperComponent implements OnInit {
-    protected isLoggedIn: boolean = false;
-    protected isUltimate: boolean = false;
+    protected isLoggedIn: boolean = false
+    protected isUltimate: boolean = false
     constructor(
         private readonly loggedInService: LoggedInService,
-        private readonly mySubscriptionTierService: MySubscriptionTierService,
+        private readonly mySubscriptionTierService: SubscriptionTierService,
+        private readonly log: CryptoTraderLoggerService,
     ) {}
 
     public ngOnInit(): void {
-        this.listenForAuthStatus();
-        this.listenForSubscriptionTier();
+        this.log.setContext(LoggerContext.Navigation)
+        this.listenForAuthStatus()
+        this.listenForSubscriptionTier()
     }
 
     private listenForAuthStatus(): void {
         this.loggedInService.getAuthState().subscribe((authStatus: boolean): void => {
-            this.isLoggedIn = authStatus;
-        });
+            this.isLoggedIn = authStatus
+        })
     }
 
     private listenForSubscriptionTier(): void {
-        this.mySubscriptionTierService.getSubscriptionTierStream().subscribe((tier: SubscriptionTier): void => {
-            this.isUltimate = tier === 'ULTIMATE';
-        });
+        this.mySubscriptionTierService
+            .getSubscriptionTier()
+            .subscribe((tier: SubscriptionTierResponse): void => {
+                this.log.log(`Observed subscription tier: ${tier.subscriptionTier}`)
+                this.isUltimate = tier.subscriptionTier === 'ULTIMATE'
+        })
     }
 }
