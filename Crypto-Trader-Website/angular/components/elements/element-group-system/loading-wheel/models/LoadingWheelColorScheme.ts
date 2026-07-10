1d748274801c6cb8977f4fb6165087a587740259
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 06:22:22 -0500

    Expand LoadingWheelColorScheme.ts to add the `TERMINAL` color option in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/models/LoadingWheelColorScheme.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/models/LoadingWheelColorScheme.ts
@@ -1,3 +1,5 @@
+// TODO: Refactor to Pascal case.
+
 /**
  * The color scheme of the loading wheel by SCSS class.
  */
@@ -5,6 +7,7 @@ export enum LoadingWheelColorScheme {
     OCEAN = 'color-ocean',
     SUNSET = 'color-sunset',
     GOLDEN = 'color-golden',
+    TERMINAL = 'color-terminal',
     EMERALD = 'color-emerald',
     FROST = 'color-frost',
 }
