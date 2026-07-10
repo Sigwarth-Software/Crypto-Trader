4f05721b4eb2fbeb367eb189b1a3f2a399480305
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 06:12:11 -0500

    Edit URL path from `/me/tier` to `/tier` in UserController.java for Crypto-Trader-Api

--- Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/UserController.java
+++ Crypto-Trader-Api/src/main/java/org/cryptotrader/api/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
     }
     //=============================-Methods-==================================
 
-    @GetMapping("/me/tier")
+    @GetMapping("/tier")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<SubscriptionTierResponse> getMySubscriptionTier(@AuthenticationPrincipal ProductUser user) {
         return ResponseEntity.ok(new SubscriptionTierResponse(user.getSubscriptionTier()));
