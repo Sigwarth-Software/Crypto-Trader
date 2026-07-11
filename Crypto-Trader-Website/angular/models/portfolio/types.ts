f788d9edb707f76fa99776b36bcd4665b3950592
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 20:55:45 -0500

    Add `PortfolioHistory` and `RangedPortfolioHistoryRequest` types to represent portfolio data structures in types.ts for Crypto-Trader-Website

--- Crypto-Trader-Website/angular/models/portfolio/types.ts
+++ Crypto-Trader-Website/angular/models/portfolio/types.ts
@@ -18,3 +18,18 @@ export type PortfolioAsset = {
     lastUpdated: string;
     vendorName: string;
 };
+
+export type PortfolioHistory = {
+    id: number
+    dollarBalance: number
+    shareBalance: number
+    totalWorth: number
+    valueChange: number
+    tradeOccurred: boolean
+    lastUpdated: string
+}
+
+export type RangedPortfolioHistoryRequest = {
+    startDate: string
+    endDate: string
+}
