aede3077c9cb0f6594d573cba7d064dae40ff476
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 20:02:51 -0500

    Expand PortfolioHistoryRepository.java to add methods for fetching portfolio history by date range and sorting by last updated timestamp in Api-Repositories

--- Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/PortfolioHistoryRepository.java
+++ Crypto-Trader-Library/Api-Library/Api-Repositories/src/main/java/org/cryptotrader/api/library/repository/PortfolioHistoryRepository.java
@@ -4,6 +4,7 @@ package org.cryptotrader.api.library.repository;
 import org.cryptotrader.api.library.entity.portfolio.PortfolioHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PortfolioHistoryRepository extends JpaRepository<PortfolioHistory, Long> {
@@ -11,6 +12,8 @@ public interface PortfolioHistoryRepository extends JpaRepository<PortfolioHisto
 
     //----------------------Find-All-By-Portfolio-Id--------------------------
     List<PortfolioHistory> findAllByPortfolioId(Long portfolioId);
+    List<PortfolioHistory> findAllByPortfolioIdOrderByLastUpdatedAsc(Long portfolioId);
+    List<PortfolioHistory> findAllByPortfolioIdAndLastUpdatedBetweenOrderByLastUpdatedAsc(Long portfolioId, LocalDateTime startDate, LocalDateTime endDate);
     //---------------------Get-First-By-Portfolio-Id--------------------------
     PortfolioHistory getFirstByPortfolioId(Long portfolioId);
     PortfolioHistory findFirstByPortfolioIdOrderByLastUpdatedDesc(Long portfolioId);
