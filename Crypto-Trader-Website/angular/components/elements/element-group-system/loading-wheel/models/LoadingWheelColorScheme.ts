e3b1de4b2723d50f4bacb6ef679b6930bd577b59
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 20:19:08 -0500

    Refactor `LoadingWheelColorScheme` enum keys in LoadingWheelColorScheme.ts to use Pascal case for consistency in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/models/LoadingWheelColorScheme.ts
+++ Crypto-Trader-Website/angular/components/elements/element-group-system/loading-wheel/models/LoadingWheelColorScheme.ts
@@ -1,13 +1,11 @@
-// TODO: Refactor to Pascal case.
-
 /**
  * The color scheme of the loading wheel by SCSS class.
  */
 export enum LoadingWheelColorScheme {
-    OCEAN = 'color-ocean',
-    SUNSET = 'color-sunset',
-    GOLDEN = 'color-golden',
-    TERMINAL = 'color-terminal',
-    EMERALD = 'color-emerald',
-    FROST = 'color-frost',
+    Ocean = 'color-ocean',
+    Sunset = 'color-sunset',
+    Golden = 'color-golden',
+    Terminal = 'color-terminal',
+    Emerald = 'color-emerald',
+    Frost = 'color-frost',
 }
