7eec4a6caf3e02702d7f04aca72e98dca804f8f5
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 20:58:39 -0500

    Expand PixelCalculatorBuilder.ts to implement `BuilderFactory`, improve type annotations, and add JSDoc comments in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/services/ui/models/PixelCalculatorBuilder.ts
+++ Crypto-Trader-Website/angular/services/ui/models/PixelCalculatorBuilder.ts
@@ -1,44 +1,74 @@
-import { type PixelCalculator } from './PixelCalculator';
-import { type PixelVariables } from './types';
+import { type PixelCalculator } from './PixelCalculator'
+import { type PixelVariables } from './types'
+import { BuilderFactory } from '@models/factory/BuilderFactory'
 
-export class PixelCalculatorBuilder {
-    private _vw = 0;
-    private _vh = 0;
-    private _rem = 0;
+/**
+ * A builder class for creating pixel variables based on viewport width (vw),
+ * viewport height (vh), and root em (rem) values.
+ */
+export class PixelCalculatorBuilder implements BuilderFactory<PixelVariables> {
+    private _vw: number = 0
+    private _vh: number = 0
+    private _rem: number = 0
 
-    constructor(private pixelCalculator: PixelCalculator) {}
+    constructor(private readonly pixelCalculator: PixelCalculator) {}
 
+    /**
+     * Sets the viewport width (vw) value.
+     * @param vw - The viewport width value to set.
+     * @returns The current instance for method chaining.
+     */
     public vw(vw: number): PixelCalculatorBuilder {
         this._vw = vw;
         return this;
     }
 
+    /**
+     * Sets the viewport height (vh) value.
+     * @param vh - The viewport height value to set.
+     * @returns The current instance for method chaining.
+     */
     public vh(vh: number): PixelCalculatorBuilder {
         this._vh = vh;
         return this;
     }
 
+    /**
+     * Sets the root em (rem) value.
+     * @param rem - The root em value to set.
+     * @returns The current instance for method chaining.
+     */
     public rem(rem: number): PixelCalculatorBuilder {
-        this._rem = rem;
-        return this;
+        this._rem = rem
+        return this
     }
 
+    /**
+     * Calculates the pixel value based on the set vw, vh, and rem values.
+     * @returns The calculated pixel value.
+     */
     public pixels(): number {
-        const vwPixels: number = this.pixelCalculator.vwToPixels(this._vw);
-        const vhPixels: number = this.pixelCalculator.vhToPixels(this._vh);
-        const remPixels: number = this.pixelCalculator.remToPixels(this._rem);
+        const vwPixels: number = this.pixelCalculator.vwToPixels(this._vw)
+        const vhPixels: number = this.pixelCalculator.vhToPixels(this._vh)
+        const remPixels: number = this.pixelCalculator.remToPixels(this._rem)
         return this.pixelCalculator.getByViewportRem(
             vwPixels,
             vhPixels,
             remPixels,
-        );
+        )
     }
 
+    /**
+     * Builds and returns the pixel variables object containing the set vw,
+     * vh, and rem values.
+     *
+     * @returns An object containing the pixel variables.
+     */
     public build(): PixelVariables {
         return {
             vw: this._vw,
             vh: this._vh,
             rem: this._rem,
-        };
+        }
     }
 }
