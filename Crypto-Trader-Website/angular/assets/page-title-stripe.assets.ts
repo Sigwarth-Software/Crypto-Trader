3b436f7b60ff67a641c1f1b14564cfef31e3434a
theoliverlear <sigwartholiver2@gmail.com>
2026-07-10 20:10:15 -0500

    Add page-title-stripe.assets.ts to define page titles and subtitles for various sections in Crypto-Trader-Website

---
+++ Crypto-Trader-Website/angular/assets/page-title-stripe.assets.ts
@@ -0,0 +1,23 @@
+import {
+    PageTitleStripe
+} from '@components/elements/element-group-system/page-title-stripe/models/PageTitleStripe'
+
+export const dashboardPageTitleStripe: PageTitleStripe = {
+    pageTitle: 'Dashboard',
+    pageSubtitle: 'Your Crypto Trader hub — everything in one place.',
+}
+
+export const statisticsPageTitleStripe: PageTitleStripe = {
+    pageTitle: 'Statistics',
+    pageSubtitle: 'Meaning behind the numbers — your performance at a glance.',
+}
+
+export const portfolioPageTitleStripe: PageTitleStripe = {
+    pageTitle: 'Portfolio',
+    pageSubtitle: `Select your assets and choose how they're traded.`,
+}
+
+export const traderPageTitleStripe: PageTitleStripe = {
+    pageTitle: 'Trader',
+    pageSubtitle: 'Your automated trades — see the profit.'
+}
