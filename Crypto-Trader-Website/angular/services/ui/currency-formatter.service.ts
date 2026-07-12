f87ab72743926eb6e497a73b422a861d22c39f89
theoliverlear <sigwartholiver2@gmail.com>
2026-07-11 19:29:19 -0500

    Expand currency-formatter.service.ts to support optional two-decimal limit and add JSDoc comments in Crypto-Trader-Website

--- Crypto-Trader-Website/angular/services/ui/currency-formatter.service.ts
+++ Crypto-Trader-Website/angular/services/ui/currency-formatter.service.ts
@@ -1,30 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
+/**
+ * A service that formats currency values.
+ */
 @Injectable({
     providedIn: 'root',
 })
 export class CurrencyFormatterService {
     constructor() {}
 
-    public formatCurrency(amount: number): string {
-        if (amount == null || isNaN(amount)) {
-            amount = 0;
+    /**
+     * Formats a currency value as a string in USD format.
+     *
+     * @param amount The currency value to format.
+     * @param limitTwoDigits Whether to force the formatted value to have at
+     * most two decimal places.
+     *
+     * @returns The formatted currency string.
+     */
+    // TODO: Replace flag with params object.
+    public formatCurrency(amount: number, limitTwoDigits: boolean = false): string {
+        if (isNaN(amount)) {
+            amount = 0
         }
-        let numDigits: number = 2;
+        let numDigits: number = 2
         if (amount > 0 && amount < 1) {
-            const parts = amount.toString().split('.');
-            const decimalSplit: string | undefined =
-                parts.length > 1 ? parts[1] : undefined;
+            const parts: string[] = amount.toString().split('.')
+            const decimalSplit: string | undefined = parts.length > 1 ? parts[1] : undefined
             if (decimalSplit && decimalSplit.length > 0) {
-                numDigits = decimalSplit.length;
+                numDigits = decimalSplit.length
             }
+        }
+        if (limitTwoDigits) {
+            numDigits = 2
         }
         numDigits = Math.max(numDigits, 2)
         const formatter: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: numDigits,
-        });
-        return formatter.format(amount);
+        })
+        return formatter.format(amount)
     }
 }
