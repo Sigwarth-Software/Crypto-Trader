59043b9002d528b3c7ac8ba3579c81c14d950308
theoliverlear <sigwartholiver2@gmail.com>
2026-07-26 18:09:31 -0500

    Refactor AdminUsersController.java to remove unused `HBox` import in Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/AdminUsersController.java
@@ -2,7 +2,6 @@ package org.cryptotrader.admin.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +10,9 @@ public class AdminUsersController extends VBox {
     @FXML
     private Label welcomeText;
     public AdminUsersController() {
-        
+
     }
-    
+
     @FXML
     private void initialize() {
         this.welcomeText.setText("Welcome to Crypto Trader Admin");
