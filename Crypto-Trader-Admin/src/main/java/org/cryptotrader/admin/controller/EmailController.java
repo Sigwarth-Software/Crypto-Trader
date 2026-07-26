71ec2d13cc00872234b64b9af4f9994b45355eb2
theoliverlear <sigwartholiver2@gmail.com>
2026-07-26 18:14:22 -0500

    Refactor EmailController.java to apply `final` in type declarations, use `this` keyword, and add TODO comment in Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/EmailController.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/controller/EmailController.java
@@ -1,6 +1,7 @@
 package org.cryptotrader.admin.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailController extends VBox {
+    // TODO: This should be a property. Not hardcoded.
     private static final String WEBMAIL_URL = "https://sscryptotrader.awsapps.com/mail";
 
     @FXML
@@ -21,12 +23,13 @@ public class EmailController extends VBox {
     @FXML
     private void initialize() {
         // Let the WebView grow and shrink within its parent
-        VBox.setVgrow(webView, Priority.ALWAYS);
-        webView.setMinSize(0, 0);
+        VBox.setVgrow(this.webView, Priority.ALWAYS);
+        this.webView.setMinSize(0, 0);
 
-        if (webView.getParent() instanceof Region parent) {
-            webView.prefWidthProperty().bind(parent.widthProperty());
-            webView.prefHeightProperty().bind(parent.heightProperty());
+        final Parent webViewParent = this.webView.getParent();
+        if (webViewParent instanceof final Region parent) {
+            this.webView.prefWidthProperty().bind(parent.widthProperty());
+            this.webView.prefHeightProperty().bind(parent.heightProperty());
         }
 
         this.webView.getEngine().load(WEBMAIL_URL);
