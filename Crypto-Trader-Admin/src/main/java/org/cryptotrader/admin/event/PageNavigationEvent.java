b3d40e11e745446af5ae2b214c4a77f8470f57ef
theoliverlear <sigwartholiver2@gmail.com>
2026-07-26 18:16:39 -0500

    Refactor PageNavigationEvent.java to include a TODO comment for relocating the class to the events package in Crypto-Trader-Admin

--- Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/event/PageNavigationEvent.java
+++ Crypto-Trader-Admin/src/main/java/org/cryptotrader/admin/event/PageNavigationEvent.java
@@ -2,5 +2,6 @@ package org.cryptotrader.admin.event;
 
 import org.cryptotrader.admin.route.AppPage;
 
+// TODO: Move to org.cryptotrader.admin.libary.events.
 public record PageNavigationEvent(AppPage appPage) {
 }
