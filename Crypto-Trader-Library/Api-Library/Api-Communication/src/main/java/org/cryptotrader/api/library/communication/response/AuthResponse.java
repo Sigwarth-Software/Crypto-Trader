cded2e7b65aab4f7b19e638893c7e74a73944c05
theoliverlear <sigwartholiver2@gmail.com>
2026-09-23 19:47:05 -0500

    Refactor AuthResponse.java to add constructors using `AuthStatus` and improve `final` parameter declarations in Api-Communication

--- Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/AuthResponse.java
+++ Crypto-Trader-Library/Api-Library/Api-Communication/src/main/java/org/cryptotrader/api/library/communication/response/AuthResponse.java
@@ -1,15 +1,27 @@
 package org.cryptotrader.api.library.communication.response;
 
 import lombok.Data;
+import org.cryptotrader.universal.library.model.http.AuthStatus;
 
 @Data
 public class AuthResponse {
     private boolean authorized;
     private String token;
-    public AuthResponse(boolean isAuthorized) {
+
+    public AuthResponse(final AuthStatus authStatus) {
+        this.authorized = authStatus.isAuthorized;
+    }
+
+    public AuthResponse(final AuthStatus authStatus, final String token) {
+        this.authorized = authStatus.isAuthorized;
+        this.token = token;
+    }
+
+    public AuthResponse(final boolean isAuthorized) {
         this.authorized = isAuthorized;
     }
-    public AuthResponse(boolean isAuthorized, String token) {
+
+    public AuthResponse(final boolean isAuthorized, final String token) {
         this.authorized = isAuthorized;
         this.token = token;
     }
